71da9dd69bd092ff931df76688c2712709213614
Einar Birkeland <einarbirkeland@gmail.com>
2024-04-24 14:24:57 +0200

    stats to rule file, df.append fixed

--- workflow/scripts/table_stats.py
+++ workflow/scripts/table_stats.py
@@ -349,26 +349,28 @@ def get_DRAMv(input):
 
 def vOTU_AMG_stats():
     """
-    Function that creates the  AMG table and aggregates the functional annotation
+    Function that creates the AMG table and aggregates the functional annotation
     from VIBRANT and DRAMv
     """
     column_names = ["protein/gene", "scaffold", "ID", "Description"]
-    df = pd.DataFrame(columns=column_names)
     dramv_amgs = pd.read_table(
         output_path + "/DRAMv/distilled/amg_summary.tsv",
         sep="\t",
         usecols=["gene", "scaffold", "gene_id", "gene_description"],
     )
-    for index, x in dramv_amgs.iterrows():
-        row = {"protein/gene": "", "scaffold": "", "ID": "", "Description": ""}
+    rows_list = []
+    for _, row in dramv_amgs.iterrows():
+        row_dict = {
+            "protein/gene": row["gene"],
+            "scaffold": row["scaffold"],
+            "ID": row["gene_id"],
+            "Description": row["gene_description"],
+        }
+        rows_list.append(row_dict)
+    
+    df = pd.DataFrame(rows_list, columns=column_names)
 
-        row["protein/gene"] = x["gene"]
-        row["scaffold"] = x["scaffold"]
-        row["ID"] = x["gene_id"]
-        row["Description"] = x["gene_description"]
-        df = df.append(row, ignore_index=True)
-
-    df.to_csv(output_path + "/statistics/vOTU_AMGs.tsv", sep="\t")
+    df.to_csv(output_path + "/statistics/vOTU_AMGs.tsv", sep="\t", index=False)
 
 
 def create_relative_Abundance():
